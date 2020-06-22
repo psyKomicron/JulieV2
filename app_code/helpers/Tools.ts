f4cc7450694fcdb5040321a2173e51513ff22a93
psyKomicron <julien@cavaweb.net>
2020-06-22 23:26:52 +0200

    added cleanHtml to tool kit

--- app_code/helpers/Tools.ts
+++ app_code/helpers/Tools.ts
@@ -27,4 +27,36 @@ export class Tools
     {
         return url.match(this.getUrlRegex()) != undefined;
     }
+
+    /**
+     * Cleans a string from html symbols, and replace them with their values.
+     * @param value String to clean
+     */
+    public static cleanHtml(text: string): string
+    {
+        // regex for html codes : /(&[A-z]+;)|(&#[0-9]+;)/g
+        // replace const symbols
+        let value = text;
+        const lookup = [
+            { "regex": /(&amp;)/g, "replace": "&" },
+            { "regex": /(&quote;)/g, "replace": "\"" }
+        ]
+        for (var i = 0; i < lookup.length; i++)
+        {
+            if (value.match(lookup[i].regex))
+            {
+                value = value.replace(lookup[i].regex, lookup[i].replace);
+            }
+        }
+        // replace ascii specials chars
+        for (var j = 32; j < 101; j++)
+        {
+            let regex = new RegExp(`&#[${j}]+;`);
+            if (value.match(regex))
+            {
+                value = value.replace(regex, String.fromCharCode(j));
+            }
+        }
+        return value;
+    }
 }
