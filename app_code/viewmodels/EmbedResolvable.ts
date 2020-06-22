66df37f2baa973272049e9df068e0b478876a13b
psyKomicron <julien@cavaweb.net>
2020-06-22 23:27:38 +0200

    moved method to bottom

--- app_code/viewmodels/EmbedResolvable.ts
+++ app_code/viewmodels/EmbedResolvable.ts
@@ -22,6 +22,61 @@ export class EmbedResolvable
     private _url: string;
     private _video: string;
 
+    public get author(): string { return this._author; }
+    public set author(value: string) { this._author = value; }
+
+    public get color(): number { return this._color; }
+    public set color(value: number) { this._color = value; }
+
+    public get createdAt(): Date { return this._createdAt; }
+    public set createdAt(value: Date) { this._createdAt = value; }
+
+    public get description(): string { return this._description; }
+    public set description(value: string) { this._description = value; }
+
+    /**Not nullable field */
+    public get fields(): Array<EmbedField> { return this._fields; }
+    public set fields(value: Array<EmbedField>) { this._fields = value; }
+
+    public get files(): any[] { return this._files; }
+    public set files(value: any[]) { this._files = value; }
+
+    public get footer(): string { return this._footer; }
+    public set footer(value: string) { this._footer = value; }
+
+    /**Not nullable field */
+    public get timestamp(): Date { return this._timestamp; }
+    public set timestamp(value: Date) { this._timestamp = value; }
+
+    public get thumbnail(): string { return this._thumbnail; }
+    public set thumbnail(value: string) { this._thumbnail = value; }
+
+    public get provider(): string { return this._provider; }
+    public set provider(value: string) { this._provider = value; }
+
+    public get length(): number { return this._length; }
+    public set length(value: number) { this._length = value; }
+
+    /**Not nullable field */
+    public get image(): string { return this._image; }
+    public set image(value: string) { this._image = value; }
+
+    public get hexColor(): string { return this._hexColor; }
+    public set hexColor(value: string) { this._hexColor = value; }
+
+    public get title(): string { return this._title; }
+    public set title(value: string) { this._title = value; }
+
+    public get type(): string { return this._type; }
+    public set type(value: string) { this._type = value; }
+
+    public get url(): string { return this._url; }
+    public set url(value: string) { this._url = value; }
+
+    public get video(): string { return this._video; }
+    public set video(value: string) { this._video = value; }
+
+
     public setAuthor(author: string)
     {
         this.author = author;
@@ -123,60 +178,4 @@ export class EmbedResolvable
         this.video = video;
         return this;
     }
-
-    public get author(): string { return this._author; }
-    public set author(value: string) { this._author = value; }
-
-    public get color(): number { return this._color; }
-    public set color(value: number) { this._color = value; }
-
-    public get createdAt(): Date { return this._createdAt; }
-    public set createdAt(value: Date) { this._createdAt = value; }
-
-    public get description(): string { return this._description; }
-    public set description(value: string) { this._description = value; }
-
-    /**Not nullable field */
-    public get fields(): Array<EmbedField> { return this._fields; }
-    public set fields(value: Array<EmbedField>) { this._fields = value; }
-
-    public get files(): any[] { return this._files; }
-    public set files(value: any[]) { this._files = value; }
-
-    public get footer(): string { return this._footer; }
-    public set footer(value: string) { this._footer = value; }
-
-    /**Not nullable field */
-    public get timestamp(): Date { return this._timestamp; }
-    public set timestamp(value: Date) { this._timestamp = value; }
-
-    public get thumbnail(): string { return this._thumbnail; }
-    public set thumbnail(value: string) { this._thumbnail = value; }
-
-    public get provider(): string { return this._provider; }
-    public set provider(value: string) { this._provider = value; }
-
-    public get length(): number { return this._length; }
-    public set length(value: number) { this._length = value; }
-
-    /**Not nullable field */
-    public get image(): string { return this._image; }
-    public set image(value: string) { this._image = value; }
-
-    public get hexColor(): string { return this._hexColor; }
-    public set hexColor(value: string) { this._hexColor = value; }
-
-    public get title(): string { return this._title; }
-    public set title(value: string) { this._title = value; }
-
-    public get type(): string { return this._type; }
-    public set type(value: string) { this._type = value; }
-
-    public get url(): string { return this._url; }
-    public set url(value: string) { this._url = value; }
-
-    public get video(): string { return this._video; }
-    public set video(value: string) { this._video = value; }
-
-
 }
