89f36fa72e8126562499dab5e3157ecda7c62471
psyKomicron <julien@cavaweb.net>
2020-06-22 23:27:04 +0200

    added culture getter

--- app_code/viewmodels/ConfigData.ts
+++ app_code/viewmodels/ConfigData.ts
@@ -3,8 +3,11 @@ export class ConfigData
 {
     private _releaseMode: string;
     private _quickness: number;
+    private _culture: string;
 
     public get release() { return this._releaseMode; }
 
     public get quickness() { return this._quickness; }
+
+    public get culture() { return this._culture; }
 }
