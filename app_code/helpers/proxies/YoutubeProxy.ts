552803ef39f1b8e5be2edf65119d3473270ec58a
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-06-26 19:38:57 +0200

    continuing proxy implementation

--- app_code/helpers/proxies/YoutubeProxy.ts
+++ app_code/helpers/proxies/YoutubeProxy.ts
@@ -1,27 +1,58 @@
 import { YoutubeQuestion } from "../../viewmodels/YoutubeQuestion";
 import { YoutubeResponse } from "../../viewmodels/YoutubeReponse";
+import { youtube_v3 } from "googleapis";
 
 export class YoutubeProxy
 {
-    private _question: YoutubeQuestion;
-    private _response: YoutubeResponse;
+    private youtubeV3: youtube_v3.Youtube;
+    private question: YoutubeQuestion;
+    private response: YoutubeResponse;
 
-    public get response() { return this._response; }
-
-    public set question(value: YoutubeQuestion) { this._question = value; }
+    public constructor(apiKey: string)
+    {
+        this.youtubeV3 = new youtube_v3.Youtube({auth: apiKey});
+    }
 
     /**
      * Just a remote call for searchWithoutParams
      * @param input
      */
-    public search(input: YoutubeQuestion): YoutubeResponse
+    public search(input: YoutubeQuestion): Promise<YoutubeResponse>
     {
         this.question = input;
         return this.searchWithoutParams();
     }
 
-    public searchWithoutParams(): YoutubeResponse
+    public async searchWithoutParams(): Promise<YoutubeResponse>
     {
+        if (this.response)
+        {
+            return this.response;
+        }
+        else
+        {
+            let searchResults = new Array<YoutubeResponse>();
+            let response = await this.youtubeV3.search.list(this.flattenQuestion());
+            response.data.items.forEach(element => 
+            {
+                searchResults.push(new YoutubeResponse()
+                .set)
+            });
+        }
         return undefined;
     }
+
+    private flattenQuestion(): Object
+    {
+        return {
+            token: this.question.token,
+            part: this.question.part,
+            order: this.question.order,
+            q: this.question.keyword,
+            type: this.question.type,
+            relevanceLanguage: this.question.relevanceLanguage,
+            maxResults: this.question.maxResults
+        }
+    }
 }
+
