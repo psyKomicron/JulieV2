f6e49b401a811a89a443d98bf8648e999fc98c99
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-06-30 00:07:46 +0200

    Create WrongYoutubeResponseType.ts

---
+++ app_code/errors/WrongYoutubeResponseType.ts
@@ -0,0 +1,10 @@
+import { ExecutionError } from "./ExecutionError";
+import { YoutubeProxy } from "../helpers/proxies/YoutubeProxy";
+
+export class WrongYoutubeResponseType extends ExecutionError
+{
+    public constructor(proxy: YoutubeProxy)
+    {
+        super("Wrong return type from youtube", proxy.name);
+    }
+}
