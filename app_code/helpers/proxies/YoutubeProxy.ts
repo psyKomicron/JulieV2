4cc47db3d22e9882914805e72754084a186054fe
psyKomicron <49617466+psyKomicron@users.noreply.github.com>
2020-06-30 00:11:03 +0200

    finished implementing proxy, 5 min cache (not done yet)

--- app_code/helpers/proxies/YoutubeProxy.ts
+++ app_code/helpers/proxies/YoutubeProxy.ts
@@ -1,62 +1,65 @@
-import { YoutubeInput } from "../../viewmodels/YoutubeQuestion";
-import { YoutubeOutput } from "../../viewmodels/YoutubeReponse";
+import { YoutubeInput } from "../../viewmodels/YoutubeInput";
 import { youtube_v3 } from "googleapis";
+import { YoutubeOutput } from "../../viewmodels/YoutubeOuput";
+import { YoutubeItem } from "../../viewmodels/copy_books/YoutubeItem";
+import { Tools } from "../Tools";
+import { WrongYoutubeResponseType } from "../../errors/WrongYoutubeResponseType";
 
 export class YoutubeProxy
 {
     private youtubeV3: youtube_v3.Youtube;
     private question: YoutubeInput;
     private response: YoutubeOutput;
+    private static cache: Array<YoutubeInput> = new Array<YoutubeInput>();
+
+    public get name() { return "youtube-proxy"; }
 
     public constructor(apiKey: string)
     {
+        setInterval(() => YoutubeProxy.cache = new Array<YoutubeInput>(), 350000);
         this.youtubeV3 = new youtube_v3.Youtube({auth: apiKey});
     }
 
-    public async search(input: YoutubeInput): Promise<youtube_v3.Schema$SearchListResponse>
+    public async search(input: YoutubeInput): Promise<YoutubeOutput>
     {
-        
-        if (this.question && input.keyword == this.question.keyword)
+        let output = new YoutubeOutput();
+        if (YoutubeProxy.cache.find(value => value.keyword == input.keyword))
         {
+            console.log("Value in cache");
             return this.response;
         }
         else
         {
-            this.question == input;
-            //let searchResults = new Array<YoutubeOutput>();
+            YoutubeProxy.cache.push(input);
+            this.question = input;
+            const youtubeUrl = "https://www.youtube.com/watch?v=";
             let response = await this.youtubeV3.search.list(this.flattenQuestion());
-            if (response.data.kind == "youtube#videoListResponse")
+            output.totalResults = response.data.pageInfo.totalResults;
+            if (response.data.kind == "youtube#searchListResponse")
             {
                 response.data.items.forEach(item =>
                 {
-
+                    output.addItem(new YoutubeItem()
+                        .setVideoURL(youtubeUrl + item.id.videoId)
+                        .setItemID(item.id.videoId)
+                        .setTitle(Tools.cleanHtml(item.snippet.title))
+                        .setKind(item.kind)
+                        .setDescription(Tools.cleanHtml(item.snippet.description))
+                        .setThumbnails([
+                            item.snippet.thumbnails.default.url,
+                            item.snippet.thumbnails.medium.url,
+                            item.snippet.thumbnails.high.url
+                        ])
+                    );
                 });
             }
             else
             {
-                throw new Error("Wrong return type from youtube");
+                throw new WrongYoutubeResponseType(this);
             }
         }
+        return output;
     }
-    /*
-    searchResults.totalResults = res.data.pageInfo.totalResults;
-        res.data.items.forEach(item =>
-        {
-
-            items.push(new YoutubeItem()
-                .setVideoURL(youtubeUrl + item.id.videoId)
-                .setItemID(item.id.videoId)
-                .setTitle(Tools.cleanHtml(item.snippet.title))
-                .setKind(item.kind)
-                .setDescription(Tools.cleanHtml(item.snippet.description))
-                .setThumbnails([
-                    item.snippet.thumbnails.default.url,
-                    item.snippet.thumbnails.medium.url,
-                    item.snippet.thumbnails.high.url
-                ])
-            );
-        })
-    */
 
     private flattenQuestion(): Object
     {
