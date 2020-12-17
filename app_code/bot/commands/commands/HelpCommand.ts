3af2f0fc2b64ef80d9b88df9c4ae43b6a4d121de
psyKomicron <julien@cavaweb.net>
2020-12-17 02:43:07 +0100

    removed unused methods, command not deprecated anymore

--- app_code/bot/commands/commands/HelpCommand.ts
+++ app_code/bot/commands/commands/HelpCommand.ts
@@ -1,20 +1,19 @@
 import { Bot } from '../../Bot';
 import { Command } from "../Command";
 import { Message, MessageEmbed } from 'discord.js';
-import { DeprecatedCommandError } from '../../../errors/command_errors/DeprecatedCommandError';
+import { EmojiReader } from '../../../dal/readers/EmojiReader';
 
 export class HelpCommand extends Command
 {
     public constructor(bot: Bot)
     {
         super("help", bot);
-        throw new DeprecatedCommandError();
     }
 
     public async execute(message: Message): Promise<void> 
     {
         let embed = new MessageEmbed()
-            .setTitle("Help")
+            .setTitle("You can find all the commands by clicking the link below" + EmojiReader.getEmoji("pointing_down"))
             .setColor(0xff0000)
             .setDescription("Help page for Julie")
             .setURL("https://github.com/psyKomicron/Julie/blob/master/README.md")
@@ -22,31 +21,5 @@ export class HelpCommand extends Command
                 { name: "Link", value: "https://github.com/psyKomicron/Julie/blob/master/README.md" }
             );
         message.channel.send(embed);
-    }
-
-    private get voteHelp(): string
-    {
-        return `\`/ vote [max number of votes] [vote reason] [channel id]
-All fields a optional, default values are :
- - max number of votes : 1
- - vote reason : Yes/No
- - channel id : where the message has been sent`;
-    }
-
-    private get downloadHelp(): string
-    {
-        return `\`/ download [number of files to download] [type of file] [channel id]\`
-All fields a optional, default values are :
- - number of files to download : 50
- - type of file : images
- - channel id : where the message has been sent`;
-    }
-
-    private get deleteHelp(): string
-    {
-        return `\`/ delete [number of messages to delete] [channel id]\`
-All fields a optional, default values are :
- - number of messages to delete : 10
- - channel id : where the message has been sent`;
     }
 }
