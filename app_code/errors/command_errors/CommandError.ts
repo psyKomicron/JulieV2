32e0b0e17e610ebfbf3502bcb1b8df8e3293bbdb
Julien Marquet <49617466+psyKomicron@users.noreply.github.com>
2020-12-26 22:58:31 +0100

    updated to support user friendly messages with inner exception for debugging

--- app_code/errors/command_errors/CommandError.ts
+++ app_code/errors/command_errors/CommandError.ts
@@ -4,8 +4,25 @@ import { Command } from "../../bot/commands/Command";
 /**Use this class to send friendly error messages to the client.*/
 export class CommandError extends ExecutionError
 {
-    public constructor(message: string, command: Command)
+    private _internalError: ExecutionError;
+
+    public constructor(message: string, command: Command, internalError: ExecutionError = null)
     {
         super(message, command.name);
+        this._internalError = internalError;
+    }
+
+    public get internalError(): ExecutionError { return this._internalError; }
+
+    public toString(): string
+    {
+        if (this.internalError)
+        {
+            return this.internalError.toString();
+        }
+        else
+        {
+            return super.toString();
+        }
     }
 }
