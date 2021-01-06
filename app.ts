fa1608a0e4832dcc739312868ff29b33130b1a67
psyKomicron <julien@cavaweb.net>
2021-01-07 00:15:21 +0100

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,17 +3,19 @@ import { Printer } from './app_code/console/Printer';
 import { StarEffect } from './app_code/console/effects/StarEffect';
 import { Bot } from './app_code/bot/discord/Bot';
 import { Config } from './app_code/dal/Config';
-import { Client } from 'discord.js';
+import { TwitterBot } from './app_code/bot/twitter/TwitterBot';
 
 Config.init();
+
 Printer.startUp();
+
 let loadingEffect = new StarEffect([-12, -1]);
 loadingEffect.start();
+
 try
 {
-    let client = new Client();
-    new Bot(loadingEffect, client);
-
+    var bot = Bot.get(loadingEffect);
+    TwitterBot.get(bot);
 }
 catch (e)
 {
