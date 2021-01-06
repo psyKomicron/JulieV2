db2d7b600a7a28b3196743112cbf647a1e4a443d
psyKomicron <julien@cavaweb.net>
2021-01-07 00:27:00 +0100

    Create ArgumentError.ts

---
+++ app_code/errors/ArgumentError.ts
@@ -0,0 +1,9 @@
+import { ExecutionError } from "./ExecutionError";
+
+export class ArgumentError extends ExecutionError
+{
+    public constructor(message: string, parameterName: string)
+    {
+        super(message + "\nParameter name :" + parameterName, ArgumentError.name);
+    }
+}
