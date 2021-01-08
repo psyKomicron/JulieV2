f55b9d028d1e311a5cb2e40141622d42017b7779
psyKomicron <julien@cavaweb.net>
2021-01-08 01:19:27 +0100

    Create CollectCommand.ts

---
+++ app_code/bot/discord/commands/commands/CollectCommand.ts
@@ -0,0 +1,24 @@
+import { Command } from "../Command";
+import { Message, TextChannel } from "discord.js";
+import { Bot } from "../../Bot";
+
+export class CollectCommand extends Command
+{
+    public constructor(bot: Bot)
+    {
+        super("collect-command", bot);
+    }
+
+    public async execute(message: Message): Promise<void> 
+    {
+        if (message.channel instanceof TextChannel)
+        {
+            this.bot.emit("collect", message.channel);
+        }
+        else
+        {
+            this.bot.emit("collect", undefined);
+        }
+    }
+
+}
