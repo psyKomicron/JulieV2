2546a5f7f5e8ebb789cf4127d0186934651e86e3
psyKomicron <julien@cavaweb.net>
2021-06-07 08:29:54 +0200

    added internalError property

--- src/errors/ExecutionError.ts
+++ src/errors/ExecutionError.ts
@@ -1,11 +1,16 @@
 /**Mother class for all errors of this application*/
 export class ExecutionError extends Error
 {
+    private _internalError: Error;
+
     public constructor(message: string, name: string)
     {
         super(message);
         this.name = name;
     }
+
+    public get internalError(): Error { return this._internalError; }
+    public set internalError(error) { this._internalError = error; }
 
     public toString(): string
     {
