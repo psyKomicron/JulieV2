5d1d188c0c239951f87649937ec9eae3555518a5
psyKomicron <julien@cavaweb.net>
2021-06-13 14:27:34 +0200

    logging update

--- src/bot/discord/commands/commands/DefaultCommand.ts
+++ src/bot/discord/commands/commands/DefaultCommand.ts
@@ -2,6 +2,7 @@ import { Bot } from '../../Bot';
 import { LogLevels, Printer } from '../../../../console/Printer';
 import { Command } from '../Command';
 import { MessageWrapper } from '../../../common/MessageWrapper';
+import { Config } from "../../../../dal/Config";
 
 export class DefaultCommand extends Command
 {
@@ -13,7 +14,7 @@ export class DefaultCommand extends Command
     public async execute(message: MessageWrapper): Promise<void> 
     {
         Printer.title("default");
-        Printer.writeLog("unknown command used (message: " + message.content + ")\n", LogLevels.Log);
-        message.reply("Unknown command !");
+        Printer.writeLog("Unknown command used (message: " + message.content + ")", LogLevels.Warning);
+        message.reply("Unknown command! Use " + Config.getPrefix() + "help to get the list of available commands.");
     }
 }
